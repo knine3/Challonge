d1953c726f517b2c79a5b0538ff6a104d7553d1b
knine3 <kawishqayyum@gmail.com>
2019-05-18 05:05:35 +0500

    Improve the parsing_json function
    Takes one less argument.

--- challonge/helper.py
+++ challonge/helper.py
@@ -5,10 +5,21 @@ def pprint(parsed):
     print(dumps(parsed, indent=2))
 
 
-def parsing_json(participants, parent, key, values2keep):
+def parsing_json(json_response, key_name, values2keep):
+
     players = dict()
 
-    for participant in participants:
-        players[participant[parent][key]] = {k: v for (k, v) in participant[parent].items() if k in values2keep}
+    '''
+    participants is a list of multiple dicts
+    Every dict consists of 1 key:value pair
+    and we want to get the key name,
+	so we are converting the dict -> list
+	then accessing the list first element.
+    '''
+
+    parent = list(json_response[1])[0]
+
+    for participant in json_response:
+        players[participant[parent][key_name]] = {k: v for (k, v) in participant[parent].items() if k in values2keep}
 
     return players
--- challonge/parties.py
+++ challonge/parties.py
@@ -5,7 +5,7 @@ from helper import pprint, parsing_json
 # Basic Data
 main_api = 'https://api.challonge.com/v1/'
 api_key = "DeSvEj7MEldecZ7U5jvjPjRtyIttIy4HGdwlcPPR"
-tournament_ID = 'sjq12ljh'
+tournament_ID = '6276700'
 
 # Preparing API Request
 url = main_api + 'tournaments/' + tournament_ID + '/participants'
@@ -20,7 +20,7 @@ participants = json.loads(response.text)
 values2keep = ['id', 'name', 'seed', 'final_rank', 'group_player_ids']
 
 
-players = parsing_json(participants, 'participant', 'name', values2keep)
+players = parsing_json(participants, 'name', values2keep)
 
 if __name__ == '__main__':
     for player in players:
