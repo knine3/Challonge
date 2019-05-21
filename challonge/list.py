eeff349bba2db58cbd10536c94ee509b8e401d75
knine3 <kawishqayyum@gmail.com>
2019-05-21 06:27:07 +0500

    Retrieve winner and runner-up from every tournament
    Adding winners and runner-ups to tournaments.csv

--- challonge/list.py
+++ challonge/list.py
@@ -1,5 +1,6 @@
 import requests as req
 from helper import pprint, json2dict
+from parties import parties
 import json
 import csv
 
@@ -17,9 +18,21 @@ tournaments = json.loads(resp.text)
 values2keep = ['id', 'name', 'completed_at', 'participants_count', 'game_name']
 tournaments_d = json2dict(tournaments, 'name', values2keep)
 
+# Retrieving winners and runners-up from all tournaments
+for tournie in tournaments_d:
+    t_ID = str(tournaments_d[tournie]['id'])
+    players = parties(t_ID, key)
+
+    for name in players:
+        if players[name]['final_rank'] == 1:
+            tournaments_d[tournie]['winner'] = name
+        elif players[name]['final_rank'] == 2:
+            tournaments_d[tournie]['runner-up'] = name
+
+
 # Writing the tournaments data to csv
 with open('tournaments.csv', mode='w+') as csv_file:
-    fieldnames = ['id', 'name', 'completed_at', 'participants_count', 'game_name']
+    fieldnames = ['id', 'name', 'completed_at', 'participants_count', 'game_name', 'winner', 'runner-up']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
