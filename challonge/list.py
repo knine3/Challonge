b760ca2556d28b3e6c95b0ef9b5d696cfc9991db
knine3 <kawishqayyum@gmail.com>
2019-05-21 05:14:56 +0500

    Write tournament data to csv file
    Now the list.py writes the tournaments data aquired from challonge to .csv file

--- challonge/list.py
+++ challonge/list.py
@@ -1,6 +1,7 @@
 import requests as req
 from helper import pprint, json2dict
 import json
+import csv
 
 url = "https://api.challonge.com/v1/tournaments.json?state=ended"
 key = "DeSvEj7MEldecZ7U5jvjPjRtyIttIy4HGdwlcPPR"
@@ -13,16 +14,15 @@ print(resp.status_code)
 
 tournaments = json.loads(resp.text)
 
-# pprint(tournaments)
-
 values2keep = ['id', 'name', 'completed_at', 'participants_count', 'game_name']
 tournaments_d = json2dict(tournaments, 'name', values2keep)
 
+# Writing the tournaments data to csv
+with open('tournaments.csv', mode='w+') as csv_file:
+    fieldnames = ['id', 'name', 'completed_at', 'participants_count', 'game_name']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-if __name__ == '__main__':
-    for name in tournaments_d:
-        if tournaments_d[name]['game_name'] is not None:
-            if 'fifa' in tournaments_d[name]['game_name'].lower():
-                print('\n#', name)
-                for y in tournaments_d[name]:
-                    print(y, ':', tournaments_d[name][y])
+    writer.writeheader()
+    for tournie in tournaments_d:
+        if 'fifa' in tournaments_d[tournie]['game_name'].lower():
+            writer.writerow(tournaments_d[tournie])
